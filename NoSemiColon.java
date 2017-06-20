45d8bfc0cc685ee10ac5004151927b7e63090046
YUVASANKARI <yuvasankari11@gmail.com>
2017-06-20 15:23:02 +0530

    Create NoSemiColon.java

---
+++ NoSemiColon.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class NoSemiColon
+{
+ public static void main (String[] args) throws java.lang.Exception
+ { 
+  if(System.out.printf("Hello World")==null)
+  {
+  }
+ }
+}
