af84cbd1aca606efde5a67f0b98a8e31a0bde6c9
YUVASANKARI <yuvasankari11@gmail.com>
2017-06-20 16:35:55 +0530

    Update VowelOrConsonant.java

--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -6,14 +6,18 @@ class VowelOrConsonant
  public static void main (String[] args) throws java.lang.Exception
  {
    Scanner s=new Scanner(System.in);
-   char c=s.nextInt(); 
+   char c=s.next().charAt(0); 
    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
    {
     System.out.println(c +" is a vowel");
    } 
+   else if(c>='B'&& c<='Z' || c>='b'&&c<='z')
+   {
+    System.out.println(c +" is a consonant");
+   } 
    else
    {
-    System.out.println(c +" is a not a vowel");
-   } 
+     System.out.println(c +" is not at an alphabet");
+   }
  }
 }
