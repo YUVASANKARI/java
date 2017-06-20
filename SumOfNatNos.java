99b987febc79e544fc727b2d440b1eabdea5652d
YUVASANKARI <yuvasankari11@gmail.com>
2017-06-20 16:58:19 +0530

    Create SumOfNatNos.java

---
+++ SumOfNatNos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SumOfNatNos
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+          int n,sum=0;
+          Scanner s=new Scanner(System.in);
+          n=s.nextInt();
+          for(int i=0;i<n;i++)
+          {
+           sum+=s.nextInt();
+          }
+          System.out.print("\nSum of given "+n+" numbers is "+sum);
+	}
+}
