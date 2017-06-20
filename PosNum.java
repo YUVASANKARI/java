79c6896ad32eb597798744f16543abdd2b534ff1
YUVASANKARI <yuvasankari11@gmail.com>
2017-06-20 14:04:16 +0530

    Create PosNum.java

---
+++ PosNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class PosNum
+{
+ public static void main (String[] args) throws java.lang.Exception
+ {
+  Scanner s=new Scanner(System.in);
+  int no=s.nextInt();
+  if(no>=0)
+   System.out.println(no+" is a positive number");
+  else
+    System.out.println(no+" is a not positive number");	
+ }
+}
