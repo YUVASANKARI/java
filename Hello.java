aa0384f013568e3f9dbfa314187b3975afac8730
YUVASANKARI <yuvasankari11@gmail.com>
2017-06-20 16:53:00 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+          for(int i=0;i<5;i++)
+             System.out.print("\nHello");
+	}
+}
