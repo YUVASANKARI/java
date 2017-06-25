8716f9533a1621a84739ab78efd49d7a2b85d6c9
YUVASANKARI <yuvasankari11@gmail.com>
2017-06-26 00:06:41 +0530

    Update myfile.java

--- myfile.java
+++ myfile.java
@@ -5,5 +5,14 @@ public class myfile
 {
   public static void main(String [] args)
   {
+    String str;
+    Scanner s=new Scanner(System.in);
+    str=s.nextLine();
+    System.out.println(str);
+    String[] s=str.split(" ");
+    for(int i=0;i<s.length;i++)
+    {
+      System.out.println(s[i]);
+    }
   }
 }
